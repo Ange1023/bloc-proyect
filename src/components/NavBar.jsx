549f7e5ee0290f3609a4388f12a22196a36453bc
Idiar <116418972+idiarj@users.noreply.github.com>
2024-05-31 18:03:01 -0400

    arreglando el css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,18 +6,10 @@ export function NavBar(){
         <div className='loginLogo'>
             <img src="/accountLogo.svg" alt="" />
             </div>
-
-        <div className='addNoteLogo'>
-            <img src="/addLogo.svg" alt="" />
-            </div>
         </section>
-        <section style={{
-            textAlign: 'center',
-            width: '60%',
-            right: '0'
-        }}>
+        <section className="infoText">
 
-            <label>Proyecto #1, Bloc de Notas, Manejo de Frameworks 2024B</label>
+            <label>Proyecto #1, Bloc de Notas, Manejo de Frameworks 2024B </label>
             <label>Idiar Chacin, Angelina Pineda</label>
         </section>
     </div>
