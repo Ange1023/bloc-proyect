3cd70fa404e62f6516a74c12435a041072823b7d
Idiar <116418972+idiarj@users.noreply.github.com>
2024-05-31 14:44:13 -0400

    mezclados ambos repos

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,26 @@ import { NotesList } from "./components/NotesList"
 import { NotesForm } from "./components/NotesForm"
 function App(){
   return (
-    <>
+    <main>
+      <div className='bar'>
+        <section className='logos'>
+        <div className='loginLogo'>Logo login</div>
+        <div className='addNoteLogo'>Add note logo</div>
+        </section>
+      </div>
+      <section className='mainSection'>
+
+      <div className='sidebar'>
+        <h1>Notes sidebar</h1>
+      </div>
+      <div className='textContent'>
       <NotesForm/>
       <NotesList/>
-    </>
+        note content
+      </div>
+      </section>
+
+    </main>
   )
 }
 
