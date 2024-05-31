5db1d62dff290a4636fd9b32bdf139783c75af64
Idiar <116418972+idiarj@users.noreply.github.com>
2024-05-31 18:19:30 -0400

    commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,11 @@
 export function NavBar(){
     return (
     <div className='navBar'>
-        <section className='logos'>
 
         <div className='loginLogo'>
             <img src="/accountLogo.svg" alt="" />
             </div>
-        </section>
+        
         <section className="infoText">
 
             <label>Proyecto #1, Bloc de Notas, Manejo de Frameworks 2024B </label>
